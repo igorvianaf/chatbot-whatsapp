f7e24d925846ba8d20159dac5ad4284fab7c018e
igorvianaf <igor.vianaf1@hotmail.com>
2025-02-17 23:15:51 -0300

    formatar mensagem e abrir link de envio no navegador

--- bot.py
+++ bot.py
@@ -1,8 +1,13 @@
 """
 Preciso automatizar minhas mensagens para clientes gostaria de saber valores, e gostaria de que entrassem em contato comigo para explicar melhor, quero poder mandar mensagens de cobrança em determinado dia com clientes com vencimento difente.
 """
-
+#lib para abrir planilhas
 import openpyxl
+#lib para formatar links para links de api
+from urllib.parse import quote
+#lib para abrir navegador
+import webbrowser
+
 # Ler planilha e guardar as informações
 planilha_teste = openpyxl.load_workbook('clientes.xlsx')
 pag_execucao = planilha_teste['Planilha1']
@@ -12,6 +17,12 @@ for linha in pag_execucao.iter_rows(min_row=2):
     nome = linha[0].value
     telefone = linha[1].value
     data = linha[2].value
-    print(nome)
-    print(telefone)
-    print(data)
+
+    #Personalizar mensagem de envio\ Formatar data
+    mensagem = f'Oi {nome}, seu boleto irá vencer em {data.strftime('%d/%m/%Y')}'
+
+#Link exemplo = https://web.whatsapp.com/send?phone=&text=
+#Criar links personalizados para enviar mensagens com base nos dados de cada cliente.
+link_mensagem = f'https://web.whatsapp.com/send?phone={telefone}&text={quote(mensagem)}'
+
+webbrowser.open(link_mensagem)
